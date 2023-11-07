8b9c9b07644336ca964aa0f4cf72494c453df8aa
hjaber <hjaberx@gmail.com>
2023-11-06 18:58:37 -0800

    *endpoint duration measuring

--- src/routes/endpoint/+page.server.ts
+++ src/routes/endpoint/+page.server.ts
@@ -1,18 +1,6 @@
 // src/routes/endpoint/+page.server.ts
 import type { PageServerLoad } from "./$types";
 
-export const load = (async ({ fetch }) => {
-  const [dynamicData, cachedData] = await Promise.all([
-    fetch("https://uuid.rocks/json"),
-    fetch("/api/uuid"),
-  ]);
-
-  const dynamicUuid: uuidJson = await dynamicData.json();
-  const cachedUuid: uuidJson = await cachedData.json();
-
-  return { cachedUuid, dynamicUuid };
-}) satisfies PageServerLoad;
-
 type uuidJson = {
   apiVersion: string;
   uuid: string;
@@ -20,4 +8,24 @@ type uuidJson = {
   is_short_uuid: false;
   is_ulid: false;
   timestamp: string;
+};
+
+export const load: PageServerLoad = async ({ fetch }) => {
+  const startTimeDynamic = performance.now();
+  const dynamicData = await fetch("https://uuid.rocks/json");
+  const endTimeDynamic = performance.now();
+
+  const startTimeCached = performance.now();
+  const cachedData = await fetch("/api/uuid");
+  const endTimeCached = performance.now();
+
+  const dynamicUuid: uuidJson = await dynamicData.json();
+  const cachedUuid: uuidJson = await cachedData.json();
+
+  return {
+    cachedUuid,
+    dynamicUuid,
+    dynamicDuration: Math.round(endTimeDynamic - startTimeDynamic),
+    cachedDuration: Math.round(endTimeCached - startTimeCached),
+  };
 };
--- src/routes/endpoint/+page.svelte
+++ src/routes/endpoint/+page.svelte
@@ -4,10 +4,13 @@
 </script>
 
 <h1>Cached Endpoint & Dynamic Load Function</h1>
+
 <h3>Dynamic load call to uuid.rocks</h3>
 <pre>{JSON.stringify(data.dynamicUuid, null, 2)}</pre>
+<p>Duration: {data.dynamicDuration} ms</p>
 
 <h3>Cached API endpoint call to uuid.rocks</h3>
 <pre>{JSON.stringify(data.cachedUuid, null, 2)}</pre>
+<p>Duration: {data.cachedDuration} ms</p>
 
 <p>courtesy of <a href="https://uuid.rocks/">uuid.rocks</a></p>
