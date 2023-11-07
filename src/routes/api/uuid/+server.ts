123c91c0d78905d84f78767fb3981a77193f4094
hjaber <hjaberx@gmail.com>
2023-11-06 18:20:24 -0800

    *endpoint prerender

--- src/routes/api/uuid/+server.ts
+++ src/routes/api/uuid/+server.ts
@@ -1,6 +1,8 @@
 // src/routes/api/uuid/+server.ts
 import { json, type RequestHandler } from "@sveltejs/kit";
 
+export const prerender = true;
+
 export const GET: RequestHandler = async ({ setHeaders }) => {
   const data = await fetch("https://uuid.rocks/json");
   const uuid: uuidJson = await data.json();
