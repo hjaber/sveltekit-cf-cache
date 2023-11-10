714ac99d6262fb7d437e1ab2b953aa718b2f578a
hjaber <hjaberx@gmail.com>
2023-11-09 16:35:03 -0800

    *dynamic remove headers

--- src/routes/dynamic/+page.svelte
+++ src/routes/dynamic/+page.svelte
@@ -8,5 +8,5 @@
 <p>courtesy of <a href="https://uuid.rocks/">uuid.rocks</a></p>
 
 <h3>Headers in load()</h3>
-<pre> "cache-control": "public, max-age=7200, stale-while-revalidate=3600", 
- "CDN-Cache-Control": "public, max-age=14400"</pre>
+<!-- <pre> "cache-control": "public, max-age=7200, stale-while-revalidate=3600", 
+ "CDN-Cache-Control": "public, max-age=14400"</pre> -->
