69e68c8f2b1efcff06ec683df1441b3bf1d52803
hjaber <hjaberx@gmail.com>
2023-11-22 15:26:59 -0800

    *worker-cache add platform.cf object

--- src/routes/api/worker-cache/+server.ts
+++ src/routes/api/worker-cache/+server.ts
@@ -2,11 +2,19 @@
 import { json, type RequestHandler } from "@sveltejs/kit";
 
 export const GET: RequestHandler = async ({ platform }) => {
-  console.log(platform);
+  const cacheTtl = 180; // Attempt to set cache TTL to 180 seconds
+
+  // The following is a speculative attempt; typically, platform.cf doesn't provide this capability.
   const response = await fetch(
-    "https://cached-db-worker.luna-heavy.workers.dev/"
+    "https://cached-db-worker.luna-heavy.workers.dev/",
+    {
+      cf: {
+        cacheTtl: cacheTtl,
+        cacheEverything: true,
+      },
+    }
   );
-  const platformObj = JSON.stringify(platform);
+  const platformObj = JSON.stringify(platform.cf);
   const data: dataJson = await response.json();
   return json({
     data,
