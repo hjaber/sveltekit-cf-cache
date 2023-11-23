716c03712886080eddd4e212926be1642adcb8c3
hjaber <hjaberx@gmail.com>
2023-11-22 16:32:13 -0800

    *worker-cache add cached/current time

--- src/routes/api/worker-cache/+server.ts
+++ src/routes/api/worker-cache/+server.ts
@@ -1,48 +1,28 @@
 // src/routes/api/worker-cache/+server.ts
 import { json, type RequestHandler } from "@sveltejs/kit";
 
-export const GET: RequestHandler = async ({ platform }) => {
+export const GET: RequestHandler = async ({ request }) => {
   const cacheTtl = 60; // set cache TTL to 1 min
 
-  // The following is a speculative attempt; typically, platform.cf doesn't provide this capability.
   const response = await fetch("https://httpbin.org/delay/0.3", {
     cf: {
       cacheTtl: cacheTtl,
       cacheEverything: true,
     },
   });
+  await response.json();
+  const country = request.headers.get("CF-IPCountry");
 
-  const data: dataJson = await response.json();
-  return json({
-    data,
+  const cachedTime = new Date().toLocaleString("en-US", {
+    timeZone: country || "UTC",
   });
-};
 
-type dataJson = {
-  args: Record<string, unknown>;
-  data: string;
-  files: Record<string, unknown>;
-  form: Record<string, unknown>;
-  headers: {
-    Accept: string;
-    "Accept-Encoding": string;
-    "Accept-Language": string;
-    "Cache-Control": string;
-    Dnt: string;
-    Host: string;
-    Pragma: string;
-    "Sec-Ch-Ua": string;
-    "Sec-Ch-Ua-Mobile": string;
-    "Sec-Ch-Ua-Platform": string;
-    "Sec-Fetch-Dest": string;
-    "Sec-Fetch-Mode": string;
-    "Sec-Fetch-Site": string;
-    "Sec-Fetch-User": string;
-    "Sec-Gpc": string;
-    "Upgrade-Insecure-Requests": string;
-    "User-Agent": string;
-    "X-Amzn-Trace-Id": string;
-  };
-  origin: string;
-  url: string;
+  const currentTime = new Date().toLocaleString("en-US", {
+    timeZone: country || "UTC",
+  });
+
+  return json({
+    cachedTime: `Cached at: ${cachedTime}`,
+    currentTime: `Current time: ${currentTime}`,
+  });
 };
