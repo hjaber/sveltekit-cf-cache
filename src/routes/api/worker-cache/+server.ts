aa6e901418f473c1292a2ac2236b21e9a38d01f0
hjaber <hjaberx@gmail.com>
2023-11-22 16:51:46 -0800

    *worker-cache fix timing

--- src/routes/api/worker-cache/+server.ts
+++ src/routes/api/worker-cache/+server.ts
@@ -4,16 +4,30 @@ import { json, type RequestHandler } from "@sveltejs/kit";
 export const GET: RequestHandler = async ({ request }) => {
   const cacheTtl = 60; // set cache TTL to 1 min
 
-  const response = await fetch("https://httpbin.org/delay/0.3", {
-    cf: {
-      cacheTtl: cacheTtl,
-      cacheEverything: true,
-    },
-  });
-  await response.json();
-  const cachedTime = new Date().toUTCString();
+  // Parallel fetching of both resources
+  const [httpbinResponse, uuidRocksResponse] = await Promise.all([
+    fetch("https://httpbin.org/delay/0.3", {
+      cf: {
+        cacheTtl: cacheTtl,
+        cacheEverything: true,
+      },
+    }),
+    fetch("https://uuid.rocks/json", {
+      cf: {
+        cacheTtl: cacheTtl,
+        cacheEverything: true,
+      },
+    }),
+  ]);
+
+  // Parsing the JSON responses
+  await httpbinResponse.json(); // Assuming this is still needed for side effects
+  const currentTimeData = await uuidRocksResponse.json();
+  const cachedTime = new Date(currentTimeData.timestamp).toUTCString();
+  const currentTime = new Date().toUTCString();
 
   return json({
-    cachedTime: `Cached at: ${cachedTime} UTC`,
+    currentTime: `Current time: ${currentTime}`,
+    cachedTime: `Cached time: ${cachedTime}`,
   });
 };
