b7bd042b057752c528ab0310fef194f721c51ce1
hjaber <hjaberx@gmail.com>
2023-12-02 11:47:07 -0800

    *packages bump, *hyperdrive to accept queries

--- cf/hyperdrive-demo/src/index.ts
+++ cf/hyperdrive-demo/src/index.ts
@@ -1,32 +1,44 @@
 import { Client } from 'pg';
 
 export interface Env {
-	// If you set another name in wrangler.toml as the value for 'binding',
-	// replace "HYPERDRIVE" with the variable name you defined.
 	HYPERDRIVE: Hyperdrive;
 }
 
 export default {
 	async fetch(request: Request, env: Env, ctx: ExecutionContext) {
-		console.log(JSON.stringify(env));
-		// Create a database client that connects to your database via Hyperdrive
-		// Hyperdrive generates a unique connection string you can pass to
-		// supported drivers, including node-postgres, Postgres.js, and the many
-		// ORMs and query builders that use these drivers.
 		const client = new Client({ connectionString: env.HYPERDRIVE.connectionString });
-
 		try {
-			// Connect to your database
 			await client.connect();
+			let sqlQuery: string | null = null;
 
-			// Test query
-			let result = await client.query({ text: 'SELECT * FROM todos' });
+			if (request.method === 'POST') {
+				try {
+					const requestBody: { query?: string } = await request.json();
+					if (requestBody && typeof requestBody.query === 'string') {
+						sqlQuery = requestBody.query;
+					}
+				} catch (e) {
+					return new Response('Invalid request body', { status: 400 });
+				}
+			} else if (request.method === 'GET') {
+				const url = new URL(request.url);
+				sqlQuery = url.searchParams.get('query');
+			} else {
+				return new Response('Invalid request method', { status: 405 });
+			}
 
-			// Return result rows as JSON
-			return Response.json(result.rows);
+			if (!sqlQuery) {
+				return new Response('No SQL query provided', { status: 400 });
+			}
+
+			let result = await client.query(sqlQuery);
+			return new Response(JSON.stringify(result.rows), { headers: { 'Content-Type': 'application/json' } });
 		} catch (e) {
-			console.log(e);
-			return Response.json({ error: JSON.stringify(e) }, { status: 500 });
+			if (e instanceof Error) {
+				console.log(e.message);
+				return new Response(JSON.stringify({ error: e.message }), { status: 500, headers: { 'Content-Type': 'application/json' } });
+			}
+			return new Response('An unexpected error occurred', { status: 500 });
 		}
 	},
 };
