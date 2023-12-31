b51f0fdc208bc3f53f4fc42c0d048496e8125054
hjaber <hjaberx@gmail.com>
2023-12-31 08:24:57 -0800

    trialing html cache headers

--- src/routes/api/rocks-cache/+server.ts
+++ src/routes/api/rocks-cache/+server.ts
@@ -10,7 +10,7 @@ export const GET: RequestHandler = async ({ platform }) => {
       cacheEverything: true,
     },
   });
-  const data: dataJson = await response.json();
+  const data = await response.json();
   return json({
     data,
   });
